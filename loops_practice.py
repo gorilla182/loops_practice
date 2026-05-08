92a2f77f021cfaa0d5bf819c53bbc7ce70394c3e
Alex <alex.apanyuk@gmail.com>
2026-05-08 17:14:44 +0300

    feat: task3

--- loops_practice.py
+++ loops_practice.py
@@ -1,5 +1,13 @@
+import random
+import time
+
 class Loops:
     TARGET = 5
+    KFC_LOAD = 85
+    ITER = 10
+    PAUSE = 0.2
+    MESSAGE = 'АСССТАНАВИТЕЕЕЕСЬ!!!'
+
     def task1(self):
         numbers = list(range(1,8))
         for n in numbers:
@@ -12,6 +20,16 @@ class Loops:
         words = [f"str{i}" for i in range(10)]
         for word in words:
             print(word, sep=', ')
+
+    def task3(self):
+        iter = 1
+        while iter <= Loops.ITER:
+            load = random.randint(0,100)
+            if load >= Loops.KFC_LOAD:
+                print(Loops.MESSAGE)
+            time.sleep(Loops.PAUSE)
+            iter+=1
+
 
 
 
